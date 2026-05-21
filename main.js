25bc992cf00e627244ac6461f29b2557a41990c9
supermanone-boop <daikihashi1127@gmail.com>
2026-05-21 21:12:32 +0900

    main.js の更新

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ const MORECRAFTS = [
     name: "McDonnell Douglas F-4 Phantom II",
     base: 3617,
     glb: "https://raw.githubusercontent.com/supermanone-boop/models/main/F4.glb",
-    rotation: [0,0,206.5],
+    rotation: [0,-15,210],
     position: [-1,0,-1],
     scale: [1,1,1]
 },
